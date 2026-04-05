20b8a5f0686ec5b467731f1954d95f4f9bf9433c
SergTsch87 <sergtsch87@gmail.com>
2026-04-05 12:39:48 +0300

    Upd: Add is_connected() in parsers/irbis_parser.py

--- parsers/irbis_parser.py
+++ parsers/irbis_parser.py
@@ -14,3 +14,29 @@
 # # Відповідає за:
 # # - парсинг html-сторінки
 # # - формування Book
+
+
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests, socket
+from bs4 import BeautifulSoup
+
+
+# ---------- Utility functions --------------------
+def is_connected():    # Для перевірки доступності інтернету перед відправленням запиту
+    try:
+        socket.create_connection(('www.google.com', 80), timeout=5)
+        return True
+    except OSError:
+        return False
+
+
+def main():
+    pass
+
+
+# для дем-ції прикладу викор-ня ф-цій цього ф-лу
+if __name__ == '__main__':
+    main()
