a5bc8d6b8ce67dcfda9d654cb1c64420ef77d5e2
SergTsch87 <sergtsch87@gmail.com>
2026-04-08 01:01:35 +0300

    Upd: Upd xpath_query in main() - irbis_parser.py

--- parsers/irbis_parser.py
+++ parsers/irbis_parser.py
@@ -26,6 +26,8 @@ import requests, socket, time
 from bs4 import BeautifulSoup
 from lxml import html
 
+from pathlib import Path
+
 # ---------- Utility functions --------------------
 def is_connected():    # Для перевірки доступности інтернету перед надсиланням запиту
     try:
@@ -62,11 +64,11 @@ def fetch(url, timeout=10):
 
 
 def op_file_write(file_path, content):
-    with open(file_path, 'w', encoding='utf-8') as f:
+    with open(file_path, 'a', encoding='utf-8') as f:
         f.write(content)
-    print(f'Content saved to {file_path}')
-
-
+    print(f'Content append to {file_path}')
+    
+    
 # Читаємо файл замість запиту до сервера
 def op_file_read(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
@@ -74,8 +76,11 @@ def op_file_read(file_path):
 
 
 def main():
+    file_path = Path('irbis_page.html')
+
     # "https://irbis.library.kr.ua/cgi-bin/irbis64r_72/cgiirbis_64.exe?C21COM=F&I21DBN=ELIB&P21DBN=ELIB&S21FMT=&S21ALL=&Z21ID="
     url_irbis = 'https://irbis.library.kr.ua/cgi-bin/irbis64r_72/cgiirbis_64.exe'
+    
     year = 2025
     S21STN = 141 # Сторінка, з якої починаємо збір даних (1 - перша сторінка)
     S21REF = 10 # Кількість результатів на сторінці (10 - за замовчуванням)
@@ -91,27 +96,16 @@ def main():
         'P21DBN': P21DBN,
         'C21COM': C21COM,
         'url_irbis_with_params': f'{url_irbis}?C21COM=S&I21DBN=KNIGI&P21DBN={P21DBN}&S21FMT=fullw&S21ALL=(%3C.%3EG%3D{year}$%3C.%3E)&FT_REQUEST=&FT_PREFIX=&Z21ID=&S21STN={S21STN}&S21REF={S21REF}&S21CNR={S21CNR}'
-    }
-    
-    
+    }                
     # https://irbis.library.kr.ua/cgi-bin/irbis64r_72/cgiirbis_64.exe?C21COM=S&I21DBN=KNIGI&P21DBN=KNIGI&S21FMT=fullw&S21ALL=(%3C.%3EG%3D2025$%3C.%3E)&FT_REQUEST=&FT_PREFIX=&Z21ID=&S21STN=141&S21REF=10&S21CNR=20
 
 
-    # body > table > tbody > tr:nth-child(4) > td.main_content > table:nth-child(5) > tbody
-
-    # !!! Наче як унікальний way!)
-    # /html/body/table/tbody/tr[4]/td[2]/table[4]/tbody
-
-    # body > table > tbody > tr:nth-child(4) > td.main_content > table:nth-child(5) > tbody > tr:nth-child(4)
-
-    response = requests.get(irbis_dict['url_irbis_with_params'])
-
-    file_path = 'irbis_page.html'
-
-        # with open(file_path, 'w', encoding='utf-8') as f:
-        #     f.write(response.text)
-        # print('HTML content saved to irbis_page.html')
-    op_file_write(file_path, response.text)
+    if not file_path.exists(): # Якщо ф-л не існує , виконуємо запит до сервера і зберігаємо результат до файлу
+        response = requests.get(irbis_dict['url_irbis_with_params'])
+            # with open(file_path, 'w', encoding='utf-8') as f:
+            #     f.write(response.text)
+            # print('HTML content saved to irbis_page.html')
+        op_file_write(file_path, response.text)
 
     # # Читаємо файл замість запиту до сервера
     # with open(file_path, 'r', encoding='utf-8') as f:
@@ -119,21 +113,58 @@ def main():
 
     html_content = op_file_read(file_path)
 
+    # ------------  Parsing block  ----------------------
+    
     tree = html.fromstring(html_content)
+    # що містить tree? - це об'єкт, який представляє структуру HTML-документа у вигляді дерева.
+    # Він дозволяє виконувати різні операції над HTML-структурою,
+    # такі як пошук елементів за допомогою XPath, отримання тексту з елементів,
+    # зміна структури документа тощо. У цьому випадку ми використовуємо tree для виконання XPath-запиту
+    # і отримання інформації про книги з HTML-сторінки.     
+    
     # print(f'HTML: {response.text}')
     print(f'Tree: {tree}')
+
     # Використовуємо XPath для отримання потрібного блоку з інформацією про книги
+    
+        # body > table > tbody > tr:nth-child(4) > td.main_content > table:nth-child(5) > tbody
+
+        # !!! Наче як унікальний way!)
+        # /html/body/table/tbody/tr[4]/td[2]/table[4]/tbody
+
+        # body > table > tbody > tr:nth-child(4) > td.main_content > table:nth-child(5) > tbody > tr:nth-child(4)
+
     # xpath_query = '/html/body/table/tbody/tr[4]/td[2]/table[4]/tbody/tr'  # Приклад XPath для отримання рядків з інформацією про книги  
-    xpath_query = f'//table[contains(., "{S21STN}.")]'  # Приклад XPath для отримання рядків з інформацією про книги  
+    # xpath_query = '/html/body/table/tbody/tr[4]/td[2]/table[4]/tbody'  # Приклад XPath для отримання рядків з інформацією про книги  
+    # xpath_query = '/html/body/table'  # Приклад XPath для отримання рядків з інформацією про книги  
+    xpath_query = '/html/body/table/tr[4]/td[2]/table[4]/tr[@width="100%"]'  # Приклад XPath для отримання рядків з інформацією про книги  
+    # xpath_query = f'//table[contains(., "{S21STN}.")]'  # Приклад XPath для отримання рядків з інформацією про книги  
+    # UnboundLocalError: cannot access local variable 'S21STN' where it is not associated with a value
+    # Як це "не існує", - коли я створив змінну S21STN перед цим кодом?
+
+    # Тут:
+    # xpath_query = '/html/body/table/tr[4]/td[2]/table[4]'
+    # Як знайти усі tr з пар-ром "width=100%" всередині цього <table>?
+    # xpath_query = '/html/body/table/tr[4]/td[2]/table[4]/tr[@width="100%"]'
     
     # Отримуємо елемент (xpath повертає список)
     books_info = tree.xpath(xpath_query)
-    print(books_info)  # Виводимо отриману інформацію про книги
+    # print(f'books_info: {books_info}')  # Виводимо отриману інформацію про книги
+    
+    file_path_2 = 'irbis_data.html'
+    for book in books_info:
+        # print(html.tostring(book, encoding='unicode'))  # Виводимо HTML-код кожного елемента з інформацією про книги
+        op_file_write(file_path_2, html.tostring(book, encoding='unicode'))  # Зберігаємо отриманий блок з інформацією про книги у файл    
+    # op_file_write(file_path_2, books_info)  # Зберігаємо отриманий блок з інформацією про книги у файл    
+    
+    # op_file_write(file_path_2, html.tostring(books_info[0], encoding='unicode'))  # Зберігаємо отриманий блок з інформацією про книги у файл    
+
     if books_info:
         tbody = books_info[0]  # Отримуємо перший елемент tbody
         # Приклад отримання тексту всіх комірок всередині цього tbody
         data = [td.text_content().strip() for td in tbody.xpath('.//td')]
         print(data)
+        # op_file_write(file_path_2, data)
 
 
 # для дем-ції прикладу викор-ня ф-цій цього ф-лу
